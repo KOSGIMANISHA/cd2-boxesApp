c38cab4960c5ec1777df0556aa9fe4a8df1dfbe3
MANISHAKOSGI <manishakosgi00@gmail.com>
2023-12-07 15:04:16 +0000

    boxes

---
+++ index.js
@@ -0,0 +1,24 @@
+const Box = (props) => {
+  //  Write your code here.
+  const { boxCls, boxText } = props;
+  return (
+    <div className={boxCls}>
+      <p className="box-text">{boxText}</p>
+    </div>
+  );
+};
+
+const element = (
+  //  Write your code here.
+  <div className="box-bg-con">
+    <h1 className="box-heading">Boxes</h1>
+
+    <div className="box-con">
+      <Box boxCls="box-small" boxText="Small" />
+      <Box boxCls="box-medium" boxText="Medium" />
+      <Box boxCls="box-large" boxText="Large" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
